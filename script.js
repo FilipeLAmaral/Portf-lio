582055d4975b3c23a83e54b083268cfc3af99c37
FILIPE LAMPUNHANI AMARAL <filipe_l_amaral@estudante.sesisenai.org.br>
2024-11-07 08:42:28 -0300

    portfolio

---
+++ script.js
@@ -0,0 +1,15 @@
+//efeito Animação no scroll
+const myObserver = new IntersectionObserver( (entrada) =>{
+ entrada.forEach((entry) =>{
+    if(entry.isIntersecting){
+        entry.target.classList.add('show')
+    } 
+    else{
+        entry.target.classList.remove('show')
+    }
+ })
+})
+
+const elements = document.querySelectorAll('.hidden')
+
+elements.forEach( (element) => myObserver.observe(element) )
